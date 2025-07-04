776f61137d80877af87d7bcab55d159b4bb5c265
Gina Saviano <gina.saviano.97@gmail.com>
2025-07-04 09:41:06 +0200

    Initial Commit

---
+++ Project-Beatcode/Project_BeatcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_BeatcodeApp.swift
+//  Project-Beatcode
+//
+//  Created by Gina Saviano on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_BeatcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
