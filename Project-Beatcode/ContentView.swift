3147691983b3a8d1dcde291a40058939f89a19ca
Gina Saviano <gina.saviano.97@gmail.com>
2025-07-04 09:53:33 +0200

    First commit and .gitignore file

--- Project-Beatcode/ContentView.swift
+++ Project-Beatcode/ContentView.swift
@@ -5,6 +5,17 @@
 //  Created by Gina Saviano on 04/07/25.
 //
 
+///WHAT I NEED TO DO:
+///Main part:
+///- a list of cells with different titles (something simple like "Cell number X")
+///- an icon/button to mark the item as favorite or remove it from favorite WITH A TAP
+///- if I tap on the cell - excluding the favorite button -  a detail view open up with the same features as the cell (title and favorite button) (maybe a modal??)
+///- the favorite status should remain synchronized between the list and the detail view
+///
+///Extra part:
+///- the UI
+///- make the app accessible
+
 import SwiftUI
 
 struct ContentView: View {
